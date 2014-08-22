8fea27c7a930bf70fd792f98e9a5228ef9383969
Paul Carey <paul.carey@ignition.it>
2014-08-22 16:07:18 +0100

    modifies gem requires

--- lib/hive/messages.rb
+++ lib/hive/messages.rb
@@ -2,9 +2,13 @@ require "virtus"
 require "roar/representer/json"
 
 require "active_support"
-require "active_support/core_ext/object/to_json"
+require "active_support/core_ext/object/json"
 
 require "hive/messages/version"
+require "hive/paths"
+require "hive/messages/configuration"
+
+
 
 require "hive/representers/job_representer"
 require "hive/messages/execution_variables_base"
