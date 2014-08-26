538972b7ba81ee5cd04d43ab272fff0a7af359ad
Paul Carey <paul.carey@ignition.it>
2014-08-26 16:40:19 +0100

    adds Hive::Messages.configure method

--- lib/hive/messages.rb
+++ lib/hive/messages.rb
@@ -8,8 +8,6 @@ require "hive/messages/version"
 require "hive/paths"
 require "hive/messages/configuration"
 
-
-
 require "hive/representers/job_representer"
 require "hive/messages/execution_variables_base"
 require "hive/messages/job"
@@ -17,5 +15,15 @@ require "hive/messages/job"
 module Hive
   module Messages
 
+    class << self
+      attr_accessor :configuration
+
+      def configure
+
+        self.configuration = Configuration.new
+        yield(configuration)
+        Hive::Paths.base = self.configuration.base_path
+      end
+    end
   end
 end
